4895dcaf578c91b68fc8d691ff41b34a6c963853
Nadir Gheyas <gm6030@myamu.ac.in>
2024-09-09 22:28:02 +0530

    Add files via upload

---
+++ binaryToDecimalconversion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int binary,pow=1,r,res=0;
+ printf("Enter any binary number\n");
+ scanf("%d",&binary);
+ printf("The Binary Number is %d\n",binary);
+ while(binary>0){
+  r=binary%10;
+  res+=r*pow;
+  pow*=2;
+  binary/=10;
+ }
+  printf("The decimal conversion is %d",res);
+}
